2666b81dd050255f5c97989d3e9bc2163ff85ad4
Leandro Piekarski <ufuww@student.kit.edu>
2018-07-20 14:55:51 +0200

    cleaner solution

--- realsense2_camera/include/base_realsense_node.h
+++ realsense2_camera/include/base_realsense_node.h
@@ -95,7 +95,6 @@ namespace realsense2_camera
                                const std::string& to);
         void publishStaticTransforms();
         void publishRgbToDepthPCTopic(const ros::Time& t, const std::map<stream_index_pair, bool>& is_frame_arrived);
-        void publishDepthPCTopic(const ros::Time& t, const std::map<stream_index_pair, bool>& is_frame_arrived);
         Extrinsics rsExtrinsicsToMsg(const rs2_extrinsics& extrinsics, const std::string& frame_id) const;
         rs2_extrinsics getRsExtrinsics(const stream_index_pair& from_stream, const stream_index_pair& to_stream);
 
@@ -156,7 +155,6 @@ namespace realsense2_camera
         std::map<stream_index_pair, std::vector<rs2::stream_profile>> _enabled_profiles;
 
         ros::Publisher _pointcloud_publisher;
-        ros::Publisher _pointcloud_depth_publisher;
         ros::Time _ros_time_base;
         bool _align_depth;
         bool _sync_frames;
